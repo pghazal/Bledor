95e37f9fdddf2b55e5ea037a79bd408d5c8900d0
Pierre Ghazal <pghazal-ext@lequipe.fr>
2017-09-06 22:39:56 +0200

    Trying the Command form (but not over yet, to be continued)

--- src/PG/PlatformBundle/Controller/CommandController.php
+++ src/PG/PlatformBundle/Controller/CommandController.php
@@ -2,6 +2,8 @@
 
 namespace PG\PlatformBundle\Controller;
 
+use PG\PlatformBundle\Entity\Command;
+use PG\PlatformBundle\Form\CommandType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,6 +13,12 @@ class CommandController extends Controller
 
     public function addAction(Request $request)
     {
-        return new Response("OK");
+        $command = new Command();
+
+        $form = $this->createForm(CommandType::class, $command);
+
+        return $this->render('PGPlatformBundle:Command:add.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 }
---
+++ src/PG/PlatformBundle/Form/CommandType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace PG\PlatformBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class CommandType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name',          TextType::class)
+            ->add('quantity',   NumberType::class)
+            ->add('save',          SubmitType::class);
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'PG\PlatformBundle\Entity\Command'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'pg_platformbundle_command';
+    }
+}
